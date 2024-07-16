d586e8228ee67b0252d2c3fbed84f39e929948b6
AzizHr <azizharkati20@gmail.com>
2024-07-16 12:36:53 +0100

    add routes for product

---
+++ laravel-restapi/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/products/sort/name', [ProductController::class, 'sortByName']);
+Route::get('/products/sort/price', [ProductController::class, 'sortByPrice']);
+Route::get('/products/filter/category/{categoryId}', [ProductController::class, 'filterByCategory']);
