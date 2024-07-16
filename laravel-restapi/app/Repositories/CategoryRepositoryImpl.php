256a9dfe7640a8a305296c46fb2131b85a435348
AzizHr <azizharkati20@gmail.com>
2024-07-16 11:36:14 +0100

    implement all methods in category repository implementation

--- laravel-restapi/app/Repositories/CategoryRepositoryImpl.php
+++ laravel-restapi/app/Repositories/CategoryRepositoryImpl.php
@@ -2,16 +2,28 @@
 
 namespace App\Repositories;
 
+use App\Models\Category;
+
 class CategoryRepositoryImpl implements CategoryRepositoryInterface {
     
+    protected $model;
+
+    public function __construct(Category $model) {
+        $this->model = $model;
+    }
+
     public function create(array $data)
     {
-        
+        return $this->model->create($data);
     }
 
     public function delete($id)
     {
-        
+        $category = $this->model->find($id);
+        if ($category) {
+            return $category->delete();
+        }
+        return false;
     }
 
 }
