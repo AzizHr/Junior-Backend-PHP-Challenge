cd698d2d1d3020b3c51dc10ab7613f6ac6996d2c
AzizHr <azizharkati20@gmail.com>
2024-07-16 11:43:37 +0100

    register repositories in app provider

--- laravel-restapi/app/Providers/AppServiceProvider.php
+++ laravel-restapi/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\CategoryRepositoryInterface;
+use App\Repositories\CategoryRepositoryImpl;
+use App\Repositories\ProductRepositoryInterface;
+use App\Repositories\ProductRepositoryImpl;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepositoryImpl::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepositoryImpl::class);
     }
 
     /**
