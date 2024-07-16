f30a8404ffa9a7d7872c6efac4469b333a37f74d
AzizHr <azizharkati20@gmail.com>
2024-07-16 12:31:33 +0100

    update category repository implementation

--- laravel-restapi/app/Repositories/ProductRepositoryImpl.php
+++ laravel-restapi/app/Repositories/ProductRepositoryImpl.php
@@ -4,27 +4,33 @@ namespace App\Repositories;
 
 use App\Models\Product;
 
-class ProductRepositoryImpl implements ProductRepositoryInterface {
+class ProductRepositoryImpl implements ProductRepositoryInterface
+{
 
     protected $model;
 
-    public function __construct(Product $model) {
+    public function __construct(Product $model)
+    {
         $this->model = $model;
     }
 
-    public function getAll() {
+    public function getAll()
+    {
         return $this->model->all();
     }
 
-    public function getById($id) {
+    public function getById($id)
+    {
         return $this->model->find($id);
     }
-    
-    public function create(array $data) {
+
+    public function create(array $data)
+    {
         return $this->model->create($data);
     }
 
-    public function update($id, array $data) {
+    public function update($id, array $data)
+    {
         $product = $this->model->find($id);
         if ($product) {
             $product->update($data);
@@ -33,12 +39,29 @@ class ProductRepositoryImpl implements ProductRepositoryInterface {
         return null;
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $product = $this->model->find($id);
         if ($product) {
             return $product->delete();
         }
         return false;
     }
-}
 
+    public function sortByName()
+    {
+        return $this->model->orderBy('name')->get();
+    }
+
+    public function sortByPrice()
+    {
+        return $this->model->orderBy('price')->get();
+    }
+
+    public function filterByCategory($categoryId)
+    {
+        return $this->model->whereHas('categories', function ($query) use ($categoryId) {
+            $query->where('id', $categoryId);
+        })->get();
+    }
+}
