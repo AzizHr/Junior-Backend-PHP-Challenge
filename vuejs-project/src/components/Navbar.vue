219cd09152b81111bd12d804b355fb36b7490d13
AzizHr <azizharkati20@gmail.com>
2024-07-16 17:21:09 +0100

    add routes in navbar component

--- vuejs-project/src/components/Navbar.vue
+++ vuejs-project/src/components/Navbar.vue
@@ -1,25 +1,22 @@
 <template>
-  <div class="bs-component">
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-      <div class="container">
-        <router-link class="navbar-brand" to="/">News</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarColor01">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/login">Login</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/register">Register</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Challenge</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+            <router-link class="nav-link active" to="/products">Products</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/add-product">Add Product</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
+</nav>
 </template>
 
 <script>
