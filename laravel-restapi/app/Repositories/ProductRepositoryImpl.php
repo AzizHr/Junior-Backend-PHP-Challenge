7c6a05ad96826819a8e9893a8b70f074f9700939
AzizHr <azizharkati20@gmail.com>
2024-07-16 11:28:09 +0100

    create a repository implementation for product

---
+++ laravel-restapi/app/Repositories/ProductRepositoryImpl.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories;
+
+class ProductRepositoryImpl implements ProductRepositoryInterface {
+
+    public function getAll()
+    {
+        
+    }
+
+    public function getById($id)
+    {
+        
+    }
+    
+    public function create(array $data)
+    {
+        
+    }
+
+    public function update($id, array $data)
+    {
+        
+    }
+
+    public function delete($id)
+    {
+        
+    }
+
+}
