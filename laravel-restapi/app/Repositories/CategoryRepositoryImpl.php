c7a41e3655b4edb05bf835fd471861c4b2fb40d7
AzizHr <azizharkati20@gmail.com>
2024-07-16 11:25:29 +0100

    create a repository implementation

---
+++ laravel-restapi/app/Repositories/CategoryRepositoryImpl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+class CategoryRepositoryImpl implements CategoryRepositoryInterface {
+    
+    public function create(array $data)
+    {
+        
+    }
+
+    public function delete($id)
+    {
+        
+    }
+
+}
