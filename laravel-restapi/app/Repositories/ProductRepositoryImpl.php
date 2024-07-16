9edba6ec037facc2644885378d94149e2bb1409c
AzizHr <azizharkati20@gmail.com>
2024-07-16 11:33:45 +0100

    implement all methods in product repository implementation

--- laravel-restapi/app/Repositories/ProductRepositoryImpl.php
+++ laravel-restapi/app/Repositories/ProductRepositoryImpl.php
@@ -2,31 +2,43 @@
 
 namespace App\Repositories;
 
+use App\Models\Product;
+
 class ProductRepositoryImpl implements ProductRepositoryInterface {
 
-    public function getAll()
-    {
-        
+    protected $model;
+
+    public function __construct(Product $model) {
+        $this->model = $model;
     }
 
-    public function getById($id)
-    {
-        
+    public function getAll() {
+        return $this->model->all();
+    }
+
+    public function getById($id) {
+        return $this->model->find($id);
     }
     
-    public function create(array $data)
-    {
-        
+    public function create(array $data) {
+        return $this->model->create($data);
     }
 
-    public function update($id, array $data)
-    {
-        
+    public function update($id, array $data) {
+        $product = $this->model->find($id);
+        if ($product) {
+            $product->update($data);
+            return $product;
+        }
+        return null;
     }
 
-    public function delete($id)
-    {
-        
+    public function delete($id) {
+        $product = $this->model->find($id);
+        if ($product) {
+            return $product->delete();
+        }
+        return false;
     }
-
 }
+
